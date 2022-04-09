02094b2c84a72d18fbf4d816bbf6324d7bbd9b9a
suman-stha <shresthasuman919@gmail.com>
2022-04-09 12:58:21 -0400

    ModelMapper

--- blog-app-apis/src/main/java/com/codewithsuman/blog/blogappapis/services/impl/UserServiceImpl.java
+++ blog-app-apis/src/main/java/com/codewithsuman/blog/blogappapis/services/impl/UserServiceImpl.java
@@ -9,6 +9,7 @@ import com.codewithsuman.blog.blogappapis.payloads.UserDto;
 import com.codewithsuman.blog.blogappapis.repositories.UserRepo;
 import com.codewithsuman.blog.blogappapis.services.UserService;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +17,8 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepo userRepo;
+    @Autowired
+    private ModelMapper modelMapper;
 
     @Override
     public UserDto createUser(UserDto userDto) {
@@ -60,25 +63,26 @@ public class UserServiceImpl implements UserService {
     }
 
     private User dtoToUser(UserDto userDto) {
-        User user = new User();
-        user.setId(userDto.getId());
-        user.setName(userDto.getName());
-        user.setEmail(userDto.getEmail());
-        user.setAbout(userDto.getAbout());
-        user.setPassword(userDto.getPassword());
+        User user = this.modelMapper.map(userDto, User.class);
+
+        // user.setId(userDto.getId());
+        // user.setName(userDto.getName());
+        // user.setEmail(userDto.getEmail());
+        // user.setAbout(userDto.getAbout());
+        // user.setPassword(userDto.getPassword());
 
         return user;
     }
 
     public UserDto userToDto(User user) {
 
-        UserDto userDto = new UserDto();
+        UserDto userDto = this.modelMapper.map(user, UserDto.class);
 
-        userDto.setId(user.getId());
-        userDto.setName(user.getName());
-        userDto.setEmail(user.getEmail());
-        userDto.setAbout(user.getAbout());
-        userDto.setPassword(user.getPassword());
+        // userDto.setId(user.getId());
+        // userDto.setName(user.getName());
+        // userDto.setEmail(user.getEmail());
+        // userDto.setAbout(user.getAbout());
+        // userDto.setPassword(user.getPassword());
 
         return userDto;
 
